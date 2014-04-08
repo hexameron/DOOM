ddbc90db65f4cca99300159881261c45cfc7172b
pi <raspberry@pi>
2014-04-08 11:13:14 +0000

    Configure.

--- config.h
+++ config.h
@@ -1,21 +1,18 @@
-/* config.h.  Generated automatically by configure.  */
-/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+/* acconfig.h for LxDoom
+ *
+ * $Id: acconfig.h,v 1.2 1999/10/02 11:43:37 cphipps Exp $
+ * Parts Copyright (C) 1993-1996 by id Software, Inc.
+ *
+ * Process this file with autoheader to produce config.h.in,
+ * which config.status then translates to config.h.  */
 
-/* Define as __inline if that's what the C compiler calls it.  */
-/* #undef inline */
+/* Define to the name of the distribution.  */
+#define PACKAGE "lxdoom"
 
-/* Define to `unsigned' if <sys/types.h> doesn't define.  */
-/* #undef size_t */
-
-/* Define if you have the ANSI C header files.  */
-#define STDC_HEADERS 1
-
-/* Define if your processor stores words with the most significant
-   byte first (like Motorola and SPARC, unlike Intel and VAX).  */
-/* #undef WORDS_BIGENDIAN */
-
-/* Define if the X Window System is missing or not being used.  */
-#define X_DISPLAY_MISSING 1
+/* Define to the version of the distribution.  */
+#define VERSION "1.4.1"
 
 /* Define to strcasecmp, if we have it */
 #define stricmp strcasecmp
@@ -67,7 +64,7 @@
 /* #undef TIMEDIAG */
 
 /* Define to be the path where Doom WADs are stored */
-#define DOOMWADDIR "/usr/local/share/games/doom"
+#define DOOMWADDIR "${prefix}/share/games/doom"
 
 /* Define to be the path to the sound server */
 #define SNDSERV_PATH "/usr/local/games/sndserv"
@@ -76,26 +73,113 @@
 #define MUSSERV_PATH "/usr/local/games/musserv"
 
 /* Define if you have library -lXext */
-#define HAVE_LIBXEXT 1
+/* #undef HAVE_LIBXEXT */
 
-/* Define if you have the <linux/joystick.h> header file.  */
+/* Define if you have the DGA library -lXxf86dga */
+/* #undef HAVE_LIBXXF86DGA */
+
+/* 
+ * $Log: acconfig.h,v $
+ * Revision 1.2  1999/10/02 11:43:37  cphipps
+ * Added autoconf options to control diagnostics
+ *
+ * Revision 1.1  1999/09/10 20:09:11  cphipps
+ * Initial revision
+ *
+ */
+
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if you have the <linux/joystick.h> header file. */
 #define HAVE_LINUX_JOYSTICK_H 1
 
-/* Define if you have the <linux/soundcard.h> header file.  */
+/* Define to 1 if you have the <linux/soundcard.h> header file. */
 #define HAVE_LINUX_SOUNDCARD_H 1
 
-/* Define if you have the <machine/soundcard.h> header file.  */
+/* Define to 1 if you have the <machine/soundcard.h> header file. */
 /* #undef HAVE_MACHINE_SOUNDCARD_H */
 
-/* Define if you have the <sys/soundcard.h> header file.  */
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/soundcard.h> header file. */
 #define HAVE_SYS_SOUNDCARD_H 1
 
-/* Define if you have the m library (-lm).  */
-#define HAVE_LIBM 1
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
 
 /* Name of package */
 #define PACKAGE "lxdoom"
 
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
 /* Version number of package */
 #define VERSION "1.4.1"
 
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+
+/* Define to 1 if the X Window System is missing or not being used. */
+/* #undef X_DISPLAY_MISSING */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
